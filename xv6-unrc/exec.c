857ab94b9cfcea5c7226415fbfcd5d7eadd68a6e
jicibils <jicibils@hotmail.com>
2017-06-08 11:50:39 -0300

    add changes in exec.c and vm.c(comment one if)

--- xv6-unrc/exec.c
+++ xv6-unrc/exec.c
@@ -55,10 +55,10 @@ exec(char *path, char **argv)
 
   // Allocate two pages at the next page boundary.
   // Make the first inaccessible.  Use the second as the user stack.
-  sz = PGROUNDUP(sz);
-  if((sz = allocuvm(pgdir, sz, sz + 2*PGSIZE)) == 0)
+  sz = PGROUNDUP(sz+PGSIZE);
+  if((sz = allocuvm(pgdir, sz, sz + PGSIZE)) == 0)
     goto bad;
-  clearpteu(pgdir, (char*)(sz - 2*PGSIZE));
+  // clearpteu(pgdir, (char*)(sz - 2*PGSIZE));
   sp = sz;
 
   // Push argument strings, prepare rest of stack in ustack.
